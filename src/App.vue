b9949c2b3a357cb1e1dbcf100f51728e9b719f85
raynandaardany <rianjuan1125@gmail.com>
2023-07-27 11:48:08 +0700

    FlowBite

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <MyComponet></MyComponet>
+  </div>
+</template>
+
+<script setup>
+import MyComponet from './MyComponet.vue';
+</script>
---
+++ src/MyComponet.vue
@@ -0,0 +1,96 @@
+<script setup>
+import { Navbar, NavbarLogo, NavbarCollapse, NavbarLink } from 'flowbite-vue'
+import { ref } from 'vue'
+import { Tabs, Tab } from 'flowbite-vue'
+import { TheCard } from 'flowbite-vue'
+const activeTab = ref('first')
+import { Carousel } from 'flowbite-vue'
+const pictures = [
+  {
+    'src': 'https://flowbite.s3.amazonaws.com/docs/gallery/featured/image.jpg',
+    'alt': 'Picture 1',
+  },
+  {
+    'src': 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg',
+    'alt': 'Picture 2',
+  },
+  {
+    'src': 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg',
+    'alt': 'Picture 3',
+  },
+  {
+    'src': 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg',
+    'alt': 'Picture 4',
+  },
+  {
+    'src': 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg',
+    'alt': 'Picture 5',
+  },
+]
+import { Footer } from 'flowbite-vue'
+</script>
+
+
+<template>
+    <Navbar>
+      <template #logo>
+        <NavbarLogo link="https://www.google.com/" alt="Flowbite logo" image-url="https://flowbite.com/docs/images/logo.svg">
+          Flowbite
+        </NavbarLogo>
+      </template>
+      <template #default="{isShowMenu}">
+        <NavbarCollapse :isShowMenu="isShowMenu">
+          <NavbarLink is-active>Home</NavbarLink>
+          <NavbarLink>Services</NavbarLink>
+          <NavbarLink>Pricing</NavbarLink>
+          <NavbarLink>Contact</NavbarLink>
+        </NavbarCollapse>
+      </template>
+      <template #menu-icon>
+        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
+      </template>
+    </Navbar>
+
+    <Carousel :pictures="pictures"></Carousel>
+
+    <tabs variant="underline" v-model="activeTab" class="p-5"> <!-- class appends to content DIV for all tabs -->
+    <tab name="Temukan" title="Temukan">
+      
+
+        <the-card variant="image" img-src="https://flowbite.com/docs/images/blog/image-1.jpg" img-alt="Desk">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">
+      Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+    </p>
+        </the-card>
+
+
+    </tab>
+    <tab name="Berita" title="Berita">
+
+
+        <the-card href="#" variant="horizontal" img-src="https://flowbite.com/docs/images/blog/image-4.jpg" img-alt="Desk">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">
+      Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+    </p>
+  </the-card>
+
+
+    </tab>
+    <tab name="Game Kasual" title="Game Kasual">
+      
+
+        <the-card href="#">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">
+      Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+    </p>
+        </the-card>
+
+
+    </tab>
+  </tabs>
+
+  <Footer></Footer>
+</template>
